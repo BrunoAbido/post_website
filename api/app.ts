7904b0876c742c9cc160f0e11405b12d1c296d0d
bruno abido <brunoabidi@gmail.com>
2023-07-18 15:18:46 -0300

    fix: mooving the mock to the data file

--- api/app.ts
+++ api/app.ts
@@ -1,81 +1,27 @@
-interface PostCard {
-  id: number;
-  title: string;
-  img: string;
-  text: string;
-};
+import { posts } from './data';
 
-function rendering (){
-  const posts: PostCard[] = [
-    {
-      id: 1,
-      title: 'The future',
-      img: 'images/post.png',
-      text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took...'
-    },
-    {
-      id: 2,
-      title: 'The future',
-      img: 'images/post.png',
-      text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took...'
-    },
-    {
-      id: 3,
-      title: 'The future',
-      img: 'images/post.png',
-      text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took...'
-    },
-    {
-      id: 4,
-      title: 'The future',
-      img: 'images/post.png',
-      text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took...'
-    },
-    {
-      id: 5,
-      title: 'The future',
-      img: 'images/post.png',
-      text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took...'
-    },
-    {
-      id: 6,
-      title: 'The future',
-      img: 'images/post.png',
-      text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took...'
-    },
-    {
-      id: 7,
-      title: 'The future',
-      img: 'images/post.png',
-      text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took...'
-    },
-  ];
-  const getdados = document.querySelector(".card_post")
-    if (getdados) {
-      getdados.innerHTML = "";
-      
+function rendering() {
+  const getdados = document.querySelector(".card_post");
+  if (getdados) {
+    getdados.innerHTML = "";
 
-      posts.map((postcard) => {
-        const postcardIten=document.createElement("div");
-        postcardIten.className="card";
-        postcardIten.innerHTML=`
+    posts.map((postcard) => {
+      const postcardIten = document.createElement("div");
+      postcardIten.className = "card";
+      postcardIten.innerHTML = `
         <a href="post_details.html?id=${postcard.id}" class="link">
-        <img src="${postcard.img}" class="card_img"/>
-        <div class="post_elements">
-          <h3 class="post_title">${postcard.title}</h3>
-          <p class="text_post">
-            ${postcard.text}
-          </p>
-          <p class="expand">Expand...</p> 
-        </div>
+          <img src="${postcard.img}" class="card_img"/>
+          <div class="post_elements">
+            <h3 class="post_title">${postcard.title}</h3>
+            <p class="text_post">
+              ${postcard.body}
+            </p>
+            <p class="expand">Expand...</p> 
+          </div>
         </a> `;
-        getdados.appendChild(postcardIten);
-       
-      });
-    }
+      getdados.appendChild(postcardIten);
+    });
+  }
 }
 
 rendering();
-
-
-
--- api/data.ts
+++ api/data.ts
@@ -1,52 +1,222 @@
-// // type Post = {
-// //     id: number;
-// //     title: string;
-// //     imageUrl: string;
-// //     body: string;
-// //   };
+export interface ComentsUsers {
+    coments: { id: number; name: string; Email: string; body: string }[];
+    postId: number;
+}
 
-//   type Comment = {
-//     id: number;
-//     postId: number;
-//     email: string;
-//     body: string;
-//   };
-  
-//   // const posts: Post[] = [
-//   //   {
-//   //     id: 1,
-//   //     title: 'The future',
-//   //     imageUrl: 'images/post.png',
-//   //     body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took...'
-//   //   },
-//   //   {
-//   //     id: 2,
-//   //     title: 'Another Post',
-//   //     imageUrl: 'images/post.png',
-//   //     body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took...'
-//   //   },
-//   // ];
-  
-//   const comments: Comment[] = [
-//     {
-//       id: 1,
-//       postId: 1,
-//       email: 'user1@example.com',
-//       body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry from data',
-//     },
-//     {
-//       id: 2,
-//       postId: 1,
-//       email: 'user2@example.com',
-//       body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry from data',
-//     },
-//     {
-//       id: 3,
-//       postId: 2,
-//       email: 'user3@example.com',
-//       body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry from data',
-//     },
-//   ];
+export interface Post {
+    id: number;
+    title: string;
+    img: string;
+    body: string;
+}
 
-//   module.exports = { posts, comments, Post, Comment };
-  
+export const ComentUsers: ComentsUsers[] = [
+    {
+        postId: 1,
+        coments: [
+            {
+            id: 1,
+            name: "Bruno Henrique",
+            Email: "brunoabidi@gmail.com",
+            body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+            },
+            {
+            id: 2,
+            name: "Lionel Messi",
+            Email: "messi10a@gmail.com",
+            body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+            },
+            {
+            id: 3,
+            name: "Maria Joana",
+            Email: "mariajoana@gmail.com",
+            body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+            },
+            {
+            id: 4,
+            name: "Andres Dalessandro",
+            Email: "dale10@gmail.com",
+            body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+            },
+            
+        ],
+        },
+
+        {
+        postId: 2,
+        coments: [
+            {
+            id: 1,
+            name: "Smith Jones",
+            Email: "smithjoneso@gmail.com",
+            body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+            },
+            {
+            id: 2,
+            name: "Taylor Williams",
+            Email: "taylor44e@gmail.com",
+            body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+            },
+            {
+            id: 3,
+            name: "Wilson",
+            Email: "wilson@gmail.com",
+            body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+            },
+        ],
+        },
+
+        {
+        postId: 3,
+        coments: [
+            {
+            id: 1,
+            name: "Thomas Brown",
+            Email: "thomasbrown@gmail.com",
+            body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+            },
+            {
+            id: 2,
+            name: "Roberts Johnson",
+            Email: "robertsjohnson@gmail.com",
+            body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+            },
+            {
+            id: 3,
+            name: "Dixon Evans",
+            Email: "dixonevans@gmail.com",
+            body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+            },
+        ],
+        },
+        
+        {
+        postId: 4,
+        coments: [
+            {
+            id: 1,
+            name: "Pedro Carlos",
+            Email: "aaliyah20023@gmail.com",
+            body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+            },
+            {
+            id: 2,
+            name: "Luiz Henrique",
+            Email: "luizgonzaga@gmail.com",
+            body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+            },
+            {
+            id: 3,
+            name: "Gabriel Santos",
+            Email: "joaonoel@gmail.com",
+            body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+            },
+        ],
+        },
+
+        {
+        postId: 5,
+        coments: [
+            {
+            id: 1,
+            name: "Henrique Paulo",
+            Email: "henriquepaulo@gmail.com",
+            body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+            },
+            {
+            id: 2,
+            name: "Helena Maria",
+            Email: "helenamaria@gmail.com",
+            body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+            },
+            {
+            id: 3,
+            name: "joaonoel",
+            Email: "joaonoel@gmail.com",
+            body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+            },
+        ],
+        },
+
+        {
+        postId: 6,
+        coments: [
+            {
+            id: 1,
+            name: "English Teacher",
+            Email: "teacherenglish@gmail.com",
+            body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+            },
+            {
+            id: 2,
+            name: "Luiz Eduardo",
+            Email: "luizeduardo@gmail.com",
+            body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+            },
+            {
+            id: 3,
+            name: "Paulo Victor",
+            Email: "jpv@gmail.com",
+            body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+            },
+        ],
+        },
+
+        {
+        postId: 7,
+        coments: [
+            {
+            id: 1,
+            name: "English Teacher",
+            Email: "teacherenglish@gmail.com",
+            body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+            }
+        ]
+        },
+
+    ];
+
+export const posts: Post[] = [
+    {
+        id: 1,
+        title: 'The future',
+        img: 'images/post.png',
+        body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took...'
+      },
+      {
+        id: 2,
+        title: 'The future',
+        img: 'images/post.png',
+        body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took...'
+      },
+      {
+        id: 3,
+        title: 'The future',
+        img: 'images/post.png',
+        body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took...'
+      },
+      {
+        id: 4,
+        title: 'The future',
+        img: 'images/post.png',
+        body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took...'
+      },
+      {
+        id: 5,
+        title: 'The future',
+        img: 'images/post.png',
+        body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took...'
+      },
+      {
+        id: 6,
+        title: 'The future',
+        img: 'images/post.png',
+        body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took...'
+      },
+      {
+      id: 7,
+      title: 'The future',
+      img: 'images/post.png',
+      body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took...'
+      },
+    ];
